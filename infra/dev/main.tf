61e221b10d5012c83205bf39ee7a9e4afe18e167
aaron lim <aaron.limse@hotmail.com>
2025-04-14 11:29:17 +0800

    git push from dev to repo

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -8,3 +8,24 @@ terraform {
     region = "us-east-1"
   }
 }
+
+
+# ---Create the ECR Repository with image scanning enabled
+# --------------------------------------------------------
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "lach"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  # Optionally, you can set encryption and lifecycle policies if needed.
+  encryption_configuration {
+    encryption_type = "AES256" # Default is AES256 encryption, you can also use KMS.
+  }
+
+  lifecycle {
+    prevent_destroy = false # Allows the resource to be destroyed
+  }
+}
